c0c3a733a1a0dcd5373b7653336226fb62adb113
Talha <mtalhamanzoor786@gmail.com>
2024-06-10 21:36:13 +0500

    Basic FE structure done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
 
+const Container = styled.div`
+  display: flex;
+  `
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <Menu />
+      <Wrapper>
+        <Navbar />
+        Card items
+      </Wrapper>
+    </Container>
   );
 }
 

ce5caf2f4c2ccaeac0896dbd19aed90f29b22146
Talha <mtalhamanzoor786@gmail.com>
2024-06-13 19:11:48 +0500

    Sidebar done

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { IoHomeSharp, IoCompass, IoGameController, IoNewspaperSharp, IoSettings, IoHelpCircleSharp } from "react-icons/io5";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoHomeSharp, IoCompass, IoGameController, IoNewspaperSharp, IoSettings, IoHelpCircleSharp, IoMdAdd } from "react-icons/io5";
 import { MdSubscriptions, MdVideoLibrary, MdHistory, MdOutlineSportsBasketball, MdLibraryMusic, MdLiveTv, MdFlag } from "react-icons/md";
 import { BiSolidMovie } from "react-icons/bi";
 import { CgDarkMode } from "react-icons/cg";
 import logo from '../images/logo.png'
 
-const Conntainer = styled.div`
+const Container = styled.div`
   flex: 1.3;
   background-color: black;
   color: aliceblue;
@@ -18,6 +19,11 @@ const Conntainer = styled.div`
   &::-webkit-scrollbar{
     display: none;
   }
+
+  @media (max-width: 992px ) {
+    display: none;
+  }
+
 `
 
 const Wrapper = styled.div`
@@ -42,6 +48,7 @@ const Item = styled.div`
   gap: 1.2rem;
   cursor: pointer;
   font-size: 1.2rem;
+  color: white;
   padding: .6rem;
   &:hover{
     background-color: rgba(255, 255, 255, 0.5);
@@ -56,8 +63,44 @@ const HR = styled.hr`
   margin: 15px 0; 
 
 `
+const LoginSec = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.2rem;
+  cursor: pointer;
+  font-size: 1.2rem;
+  color: white;
+  padding: .6rem;
+  
+  /* &:hover{
+    background-color: rgba(255, 255, 255, 0.5);
+    background-color: hsla(0, 0%, 00%, 0.3);
+  } */
+`
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  font-size: 1rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+  transition: all .3s ease;
+  z-index: 5;
+  
+  &:hover{
+    box-shadow: 0 0 10px 5px #3ea6ff;
+  }
+`
+
 const Menu = () => {
-  const navMenu = [
+
+  const navMainItems = [
     {
       title: "Home",
       icon: <IoHomeSharp />
@@ -78,6 +121,8 @@ const Menu = () => {
       title: "History",
       icon: <MdHistory />
     },
+  ]
+  const navCatItems = [
     {
       title: "Music",
       icon: <MdLibraryMusic />
@@ -102,6 +147,8 @@ const Menu = () => {
       title: "Live",
       icon: <MdLiveTv />
     },
+  ]
+  const navHelpers = [
     {
       title: "Settings",
       icon: <IoSettings />
@@ -121,28 +168,49 @@ const Menu = () => {
   ]
 
   return (
-    <Conntainer>
+    <Container>
       <Wrapper>
         <Logo>
           <Img src={logo} />
           <span>Devido</span>
         </Logo>
         {
-          navMenu.map((item, index) => (
-            <div key={index}>
-              <Link to={`/${item.title.toLowerCase()}`} key={index} style={{ textDecoration: "none", color: "none" }}>
-                <Item>
-                  {item.icon}
-                  {item.title}
-                </Item>
-              </Link>
-
-              {(index === 5) && <HR />}
-            </div>
+          navMainItems.map((item, index) => (
+            <Link to={`/${item.title.toLowerCase()}`} key={index} style={{ textDecoration: "none", color: "none" }}>
+              <Item>
+                {item.icon}
+                {item.title}
+              </Item>
+            </Link>
+          ))
+        }
+        <HR />
+        <LoginSec>
+          <p>Log in to like, comment, and subscribe</p>
+          <Button>
+            <FaRegUserCircle /> Login
+          </Button>
+        </LoginSec>
+        <HR />
+        {
+          navCatItems.map((item, index) => (
+            <Item key={index}>
+              {item.icon}
+              {item.title}
+            </Item>
+          ))
+        }
+        <HR />
+        {
+          navHelpers.map((item, index) => (
+            <Item key={index}>
+              {item.icon}
+              {item.title}
+            </Item>
           ))
         }
       </Wrapper>
-    </Conntainer>
+    </Container>
   )
 }
 
